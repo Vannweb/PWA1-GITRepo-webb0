55589dbc260a24a4275e2fad02a8f0f316c72d9c
Vannweb <dawebb@fullsail.edu>
2015-05-16 20:09:49 -0700

    finished the week 2 assignments

---
+++ Wk1/goal1_assign_duel1 FINAL/js/main.js
@@ -0,0 +1,70 @@
+/**
+ **Devante A. T. Webb
+ **5/16/2015
+ Assignment 1
+ Part 2/3 of series
+*/
+
+// self-executing function
+(function(){
+
+    console.log("FIGHT!!!");
+
+// two arrays that contain the information of both players
+
+    var player1 = ["Spiderman",20,100];
+    var player2 = ["Batman",20,100];
+
+    //initiate round
+    var round=0;
+
+    function fight(){
+        alert(player1[0]+":"+player1[2]+"  *START*  "+player2[0]+":"+player2[2]);
+        for (var i = 0; i < 10; i++)
+        {
+            //random formula is - Math.floor(Math.random() * (max - min) + min);
+            var minDamage1 = player1[1] * .5;
+            var minDamage2 = player2[1] * .5;
+            var f1 = Math.floor(Math.random()*(player1[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2[1]-minDamage2)+minDamage2);
+
+            //inflict damage
+            player1[2]-=f1;
+            player2[2]-=f2;
+
+            console.log(player1[0]+": "+player1[2] + " " + player2[0]+":"+player2[2]);
+
+            //check for victor
+            var result = winnerCheck();
+            console.log(result);
+            if (result==="no winner")
+            {
+                round++;
+                alert(player1[0]+":"+player1[2]+"  *ROUND "+round+" OVER"+"*  "+player2[0]+":"+player2[2]);
+
+            } else{
+                alert(result);
+                break;
+            };
+
+          };
+    };
+
+    function winnerCheck(){
+        var result="no winner";
+        if (player1[2]<1 && player2[2]<1)
+        {
+            result = "You Both Die";
+        } else if(player1[2]<1){
+            result =player2[0]+" WINS!!!"
+        } else if (player2[2]<1)
+        {
+            result = player1[0]+" WINS!!!"
+        };
+       return result;
+    };
+
+    /*******  The program gets started below *******/
+    fight();
+
+})();
